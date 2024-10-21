888318accf020a71b574c6b2b74b4dc2046167c8
Marie Zirzlaff <marie.zirzlaff@dci-student.org>
2024-10-21 12:09:48 +0200

    Header

--- Adventscalender/src/components/Header.jsx
+++ Adventscalender/src/components/Header.jsx
@@ -1,3 +1,17 @@
+import React from "react"
+
 export const Header = () => {
-    return
+    return (
+        <div className="header" >
+            <img src="" alt="Bild vom Weihnachtsmann" className="header-background" />
+            <h1 className="header-title" >Adventskalender</h1>
+            <nav className="header-menue" >
+                <ul className="header-menue-list" >
+                    <li className="header-menue-item" ><a href="#">Startseite</a></li>
+                    <li className="header-menue-item" ><a href="#">Kontakt</a></li>
+                    <li className="header-menue-item" ><a href="#">Impressum</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
