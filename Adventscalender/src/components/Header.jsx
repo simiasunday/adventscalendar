f88de607d59bbd87f193c06716023a2c752005ce
Malouzi <marie.zirzlaff@dci-student.org>
2024-10-22 12:26:49 +0200

    Header Vertikal angeordnet

--- Adventscalender/src/components/Header.jsx
+++ Adventscalender/src/components/Header.jsx
@@ -5,13 +5,7 @@ export const Header = () => {
     return (
         <div className="header" >
                     <h1 className="header-title" >Adventskalender</h1>
-            <nav className="header-menue" >
-                <ul className="header-menue-list" >
-                    <li className="header-menue-item" ><a href="#">Startseite</a></li>
-                    <li className="header-menue-item" ><a href="#">Kontakt</a></li>
-                    <li className="header-menue-item" ><a href="#">Impressum</a></li>
-                </ul>
-            </nav>
+                    
         </div>
     )
 }
