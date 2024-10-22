0236b4809de62daa817e494580517adb14e7b762
Malouzi <marie.zirzlaff@dci-student.org>
2024-10-22 09:51:48 +0200

    Header und css

--- Adventscalender/src/App.jsx
+++ Adventscalender/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Calendar } from "./components/Calendar";
-import { useState } from 'react'
 import './App.css'
 
 function App() {
--- Adventscalender/src/components/Header.jsx
+++ Adventscalender/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react"
+import '../App.css'
 
 export const Header = () => {
     return (
         <div className="header" >
-            <img src="" alt="Bild vom Weihnachtsmann" className="header-background" />
-            <h1 className="header-title" >Adventskalender</h1>
+                    <h1 className="header-title" >Adventskalender</h1>
             <nav className="header-menue" >
                 <ul className="header-menue-list" >
                     <li className="header-menue-item" ><a href="#">Startseite</a></li>
