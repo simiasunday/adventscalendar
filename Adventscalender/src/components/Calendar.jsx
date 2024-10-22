07afa11997e9c6d89a9e5713e3d7a28401a747f3
simisunday <simia_karegar@live.de>
2024-10-22 11:10:59 +0200

    wichteltüren eingesetzt in css

--- Adventscalender/src/components/Calendar.jsx
+++ Adventscalender/src/components/Calendar.jsx
@@ -3,30 +3,30 @@ import React from "react";
 export const Calendar = () => {
     return (
     <div className="calendar" >
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>    
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
-        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/1.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/2.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/6.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/4.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/5.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/3.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/7.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/8.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/9.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/10.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/11.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/12.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/13.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/14.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/15.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/16.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/17.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/18.png" alt="" /></div></a>    
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/19.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/20.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/21.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/22.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/23.png" alt="" /></div></a>
+        <a href="" target="_blank" rel=""><div className="door"><img src="src/assets/24.png" alt="" /></div></a>
     </div>
     )
 }
