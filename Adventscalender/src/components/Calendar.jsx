aa83ac70d1d9d9315f355781baa05908b8ef59bd
simisunday <simia_karegar@live.de>
2024-10-22 10:02:16 +0200

    in App.jsx a tags hinzugefügt x 24

--- Adventscalender/src/components/Calendar.jsx
+++ Adventscalender/src/components/Calendar.jsx
@@ -1,3 +1,32 @@
+import React from "react";
+
 export const Calendar = () => {
-    return
+    return (
+    <div className="calendar" >
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>    
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+        <a href="" target="_blank" rel=""><div className="door"></div></a>
+    </div>
+    )
 }
