27ac84ef86d58c409996f79e2845a5720f977089
simisunday <simia_karegar@live.de>
2024-10-24 11:10:01 +0200

    const doors erstellt und map erstellt

--- Adventscalender/src/components/Calendar.jsx
+++ Adventscalender/src/components/Calendar.jsx
@@ -12,15 +12,17 @@ export const Calendar = () => {
       newFlippedDoors[index] = !newFlippedDoors[index];
       setFlippedDoors(newFlippedDoors);
     }
-    const doors = [
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-      21, 22, 23,
-    ];
-    const openDoor = door.map((door) => door, index + 1());
+
+    const openDoor = doors.map((door) => door, index + 1());
   };
 
-  return (
-    <div className="calendar">
+  const doors = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23,
+  ];
+
+  return  <>
+    {doors.map((door) => <div className="calendar">
       <a
         onClick={(e) => {
           e.preventDefault();
@@ -36,7 +38,10 @@ export const Calendar = () => {
         </div>
       </a>
     </div>
-  );
+    )}
+
+    
+    </>
 };
 
 {
